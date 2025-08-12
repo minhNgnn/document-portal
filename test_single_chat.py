16f003f5fe56832825572f31e6262e634ffad5fb
Minh Nguyen <tuanminh10082002gv@gmail.com>
2025-08-12 12:07:05 +0200

    test single chat rag

--- test_single_chat.py
+++ test_single_chat.py
@@ -1,13 +1,42 @@
 import sys
 from pathlib import Path
+from langchain_community.vectorstores import FAISS
+from src.single_doc_chat.data_ingestion import SingleDocIngestor
+from src.single_doc_chat.retriever import ConversationRAG
+from utils.model_loader import ModelLoader
+
+FAISS_INDEX_PATH = Path("faiss_index")
 
 
 def test_conversation_rag(pdf_path: str, question: str):
     try:
-        pass
+        model_loader = ModelLoader(pdf_path)
+
+        if FAISS_INDEX_PATH.exists():
+            print("Loading FAISS index...")
+            embeddings = model_loader.load_embeddings()
+            vector_store = FAISS.load_local(
+                FAISS_INDEX_PATH, embeddings, allow_dangerous_deserialization=True
+            )
+            retriever = vector_store.as_retriever(
+                search_type="similarity", search_kwargs={"k": 5}
+            )
+        else:
+            # Ingest doc and create retriever
+            print("FAISS index not found. Ingesting document...")
+            with open(pdf_path, "rb") as f:
+                uploaded_files = [f]
+                ingestor = SingleDocIngestor()
+                retriever = ingestor.ingest_files(uploaded_files)
+        print("Running Conversational RAG...")
+        session_id = "test_session"
+        rag = ConversationRAG(retriever, session_id)
+
+        response = rag.invoke(question)
+        print(f"\nQuestion :{question} \nResponse: {response}")
     except Exception as e:
         print(f"Error during test: {e}")
-        raise
+        sys.exit(1)
 
 
 if __name__ == "__main__":
