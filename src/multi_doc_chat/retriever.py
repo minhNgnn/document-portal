df88c3528a7fcf9e219a789d458e1e462c1229e1
Minh Nguyen <tuanminh10082002gv@gmail.com>
2025-08-12 18:21:19 +0200

    init conversationRag

--- src/multi_doc_chat/retriever.py
+++ src/multi_doc_chat/retriever.py
@@ -1,6 +1,34 @@
+import sys
+import os
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from exception.custom_exception import DocumentPortalException
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain, create_history_aware_retriever
+from logger.custom_logger import CustomLogger
+from model.models import PromptType, Chat
+from prompts.prompt_library import PROMPT_REGISTRY
+from utils.model_loader import ModelLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.chat_message_histories import ChatMessageHistory
+
 class ConversationalRAG:
-    def __init__(self):
-        pass
+    def __init__(self, session_id: str, retriever = None):
+        try:
+            self.log = CustomLogger().get_logger(__name__)
+            self.session_id = session_id
+            self.llm = self._load_llm()
+            self.contextualize_prompt = PROMPT_REGISTRY[PromptType.CONTEXTUALIZE_QUESTION.value]
+            self.qa_prompt = PROMPT_REGISTRY[PromptType.CONTEXT_QA.value]
+
+            if retriever is None:
+                raise ValueError("Retriever is required")
+            self.retriever = retriever
+            self._build_lcel_chain()
+            self.log.info(f"ConversationalRAG initialized successfully with session_id={session_id}")
+            
+        except Exception as e:
+            self.log.error(f"Error initializing ConversationalRAG: {e}")
+            raise DocumentPortalException("Failed to initialize ConversationalRAG")
 
     def load_retriever_from_faiss(self):
         pass
